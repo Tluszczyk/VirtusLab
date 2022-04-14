51a9fc7801ef36ab65f85b230607e419d337ccdc
Filip Tłuszcz <tluszczykin4matyk@gmail.com>
2022-04-14 14:04:22 +0200

    add main v2

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from sort_csv import sort_csv
+import sys
+from join_csv import join_csv
 
 if __name__ == '__main__':
-    a_file_path = "./data/d_10/A.csv"
-    b_file_path = "./data/d_10/B.csv"
-    column = "id"
-
-    sort_csv(a_file_path, column)
-    sort_csv(b_file_path, column)
+    a_file_path = sys.argv[1]
+    b_file_path = sys.argv[2]
+    column = sys.argv[3]
+    join_type = sys.argv[4]
+    
+    join_csv(a_file_path, b_file_path, column, join_type)
